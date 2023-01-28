24c5cdbc5b2233d1f240062733ef75eafdc55e6f
halid <barzaev97@gmail.com>
2023-01-28 19:55:40 +0300

    Сделал компонент header

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,59 +2,18 @@ import { Avatar, Button, Paper, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid/Grid';
 import React from 'react';
 import Tweet from './Tweet';
-import { TextareaAutosize } from '@mui/base';
 import { styled } from '@mui/material/styles';
+import Header from './Header';
 
-import CropOriginalIcon from '@mui/icons-material/CropOriginal';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
-const CustomTextareaAutosize = styled(TextareaAutosize)(() => ({
-  border: 'none',
-  resize: 'none',
-  width: '100%',
-  padding: '10px',
-  fontSize: '22px',
-  "&:focus": {
-    border: 'none'
-  },
-  "&:focus-visible": {
-    outline: 'none'
-  }
-}));
 
-const CustomAddTweetBlock = styled('div')(() => ({
-  display:  'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  paddingLeft: '7px'
-}))
+
+
 const Content = () => {
   return (
     <Paper sx={{ height: '100%', borderRadius: 0, borderBottom: 0, borderTop: 0, }}>
-      <Paper variant='outlined' sx={{ borderRadius: 0, border: 0, borderBottom: '1px solid lightgray', padding: '10px 15px', position: 'sticky', top: '0px', zIndex: '1',  }}>
-        <Typography variant='h6' fontWeight={700}> Глваная
-        </Typography>
-        {/* <AutoAwesomeOutlinedIcon color='primary'/> */}
-      </Paper>
-      <Paper variant='outlined' sx={{ borderRadius: 0, border: 0, borderBottom: '1px solid lightgray', padding: '10px 15px', paddingBottom: '25px' }}>
-        <Grid container>
-          <Grid item xs={1}>
-            <Avatar sx={{ marginTop: '5px' }} alt={`Аватарка пользователя `} />
-          </Grid>
-          <Grid item xs={11}>
-            <CustomTextareaAutosize minRows={1} placeholder='Что происходит ?' />
-            <CustomAddTweetBlock>
-            <div>
-            <CropOriginalIcon color='primary'/>
-            <SentimentSatisfiedAltIcon color='primary' sx={{marginLeft: '20px'}}/>
-            </div>
-            <Button color='primary' variant='contained'  >Твитнуть</Button>
-
-            </CustomAddTweetBlock>
-          </Grid>
-        </Grid>
-      </Paper>
-
+     
+      <Header/>
       {
         [...new Array(10)].fill(
           <Tweet user={{
