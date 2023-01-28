1833688944bf96bb1d655cba9a95fabdd3c55a01
halid <barzaev97@gmail.com>
2023-01-28 18:49:54 +0300

    Сделал небольшой адаптив Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Grid, IconButton, Typography } from '@mui/material';
+import { Button, Grid, IconButton, Typography } from '@mui/material';
 import React from 'react';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import TagIcon from '@mui/icons-material/Tag';
@@ -10,80 +10,83 @@ import ListAltIcon from '@mui/icons-material/ListAlt';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { styled } from '@mui/material/styles';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
+import SearchIcon from '@mui/icons-material/Search';
+import CreateIcon from '@mui/icons-material/Create';
 const List = styled('ul')(({ }) => ({
   margin: 0,
   padding: 0,
-  listStyle: 'none'
+  listStyle: 'none',
+  position: 'fixed'
 }));
-const ListItem = styled('li')(({ }) => ({
-  fontWeight: 700,
+const ListItem = styled('li')(({ theme }) => ({
+  cursor: 'pointer',
+  fontWeight: 400,
   display: 'flex',
   alignItems: 'center',
+  padding: '12px',
+  transition: 'all .10s  ease-in-out',
+  borderRadius: 30,
   "&:hover": {
-    color: 'red'
-  }
+    backgroundColor: 'rgba(15, 20, 25, 0.1)',
+    color: '#1976d2'
+  },
+
 }));
 
+
+
 const SideBar: React.FC = (): React.ReactElement => {
+  const matches = useMediaQuery('(min-width:1300px)');
+  const widthInSearch = useMediaQuery('(min-width: 1000px)')
+
   return (
-    <Grid item xs={3}>
-          <List>
-            <ListItem>
-              <IconButton sx={{ margin: '15px 0' }}>
-                <TwitterIcon sx={{ fontSize: 40 }} color='primary' />
-              </IconButton>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <HomeIcon color='primary' sx={{ fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} color='primary' sx={{ cursor: 'pointer' }} ml="12px">Главная</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <TagIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Поиск</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <NotificationsNoneIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Уведомления</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <MailOutlineIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Сообщения</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <BookmarkBorderIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Закладки</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <ListAltIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Списки</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <PersonOutlineIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Профиль</Typography>
-            </ListItem>
-            <ListItem>
-              <IconButton>
-                <MoreHorizIcon sx={{ color: 'black', fontSize: 30 }} />
-              </IconButton>
-              <Typography fontWeight='inherit' fontSize={19} ml="12px">Еще</Typography>
-            </ListItem>
-          </List>
-        </Grid>
+    <Grid item xs={matches ? 3 : 1}>
+      <List sx={{paddingLeft: !matches ? '30px !important' : '0 !important'  }}>
+        <ListItem sx={{ background: 'none !important',  padding: 0, paddingLeft: '1px !important'  }}>
+          <IconButton sx={{ margin: '15px 0' ,}}>
+            <TwitterIcon sx={{ fontSize: 30,}} color='primary' />
+          </IconButton>
+        </ListItem>
+        <ListItem>
+          <HomeIcon  sx={{ fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19}  sx={{ cursor: 'pointer' }} ml="12px">Главная</Typography>}
+        </ListItem>
+        <ListItem>
+          {widthInSearch ? <TagIcon sx={{  fontSize: 30 }} />
+            : <SearchIcon sx={{ fontSize: 30 }} />}
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Поиск</Typography>}
+        </ListItem>
+        <ListItem>
+          <NotificationsNoneIcon sx={{ fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Уведомления</Typography>}
+        </ListItem>
+        <ListItem>
+          <MailOutlineIcon sx={{  fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Сообщения</Typography>}
+        </ListItem>
+        <ListItem>
+          <BookmarkBorderIcon sx={{  fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Закладки</Typography>}
+        </ListItem>
+        <ListItem>
+          <ListAltIcon sx={{  fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Списки</Typography>}
+        </ListItem>
+        <ListItem>
+          <PersonOutlineIcon sx={{  fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Профиль</Typography>}
+        </ListItem>
+        <ListItem>
+          <MoreHorizIcon sx={{  fontSize: 30 }} />
+          {matches && <Typography fontWeight='inherit' fontSize={19} ml="12px">Еще</Typography>}
+        </ListItem>
+      
+        { matches ? <Button color='primary' variant='contained' fullWidth sx={{marginTop: '30px', padding: '10px'}}>Твитнуть</Button>
+        :<Button color='primary' variant='contained' fullWidth sx={{marginTop: '30px', padding: '10px', width: '30px', height: '40px', minWidth: '40px',minHeight: '100%', marginLeft: '7px'}}><CreateIcon /></Button> }
+      </List>
+      
+    </Grid>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,28 +21,26 @@ const SearchTextField = styled(InputBase)({
 
 const Home: React.FC = () => {
   return (
-    <Container maxWidth="lg" sx={{ height: '100vh' }} >
-      <Grid sx={{ flexGrow: 1, }} container spacing={3}  >
-       <SideBar/>
-      
+    <Container maxWidth="lg" sx={{ height: '100vh' }}>
+      <Grid sx={{ flexGrow: 1, }} container spacing={3}>
+          <SideBar />
         <Grid item xs={6}>
           <Paper sx={{ height: '100%', borderRadius: 0, borderBottom: 0, borderTop: 0, }} variant="outlined">
             <Paper variant='outlined' sx={{ borderRadius: 0, border: 0, borderBottom: '1px solid lightgray', padding: '10px 15px' }}>
-              <Typography variant='h6'  fontWeight={700}> Глваная
+              <Typography variant='h6' fontWeight={700}> Глваная
               </Typography>
               {/* <AutoAwesomeOutlinedIcon color='primary'/> */}
             </Paper>
-           
             {
-        [...new Array(10)].fill(
-          <Tweet user={{
-            fullname: 'Халид',
-            username: 'Баз',
-            avatarUrl: "https://images.unsplash.com/photo-1571566882372-1598d88abd90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-          }} text={'Фонд Банги @BungieLove · 29 дек. 2022 г. В прямом эфире с @TheLostDrake сейчас! Присоединяйтесь к нам, пока мы играем @DestinyTheGame и поблагодарить @Бунги сообщество для потрясающей #Game2Give, раздавая множество отличных призов! Swag, Lightfall и даже студийный тур!'}
-          />
-        )
-       }
+              [...new Array(10)].fill(
+                <Tweet user={{
+                  fullname: 'Халид',
+                  username: 'Баз',
+                  avatarUrl: "https://images.unsplash.com/photo-1571566882372-1598d88abd90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                }} text={'Фонд Банги @BungieLove · 29 дек. 2022 г. В прямом эфире с @TheLostDrake сейчас! Присоединяйтесь к нам, пока мы играем @DestinyTheGame и поблагодарить @Бунги сообщество для потрясающей #Game2Give, раздавая множество отличных призов! Swag, Lightfall и даже студийный тур!'}
+                />
+              )
+            }
           </Paper>
         </Grid>
         <Grid item xs={3}>
